e20b12b86614198db336b0a34ef9ea34c08cb616
KaraboMmako <karabommakokm20@gmail.com>
2025-04-20 15:01:01 +0200

    Updated the start.js file

--- backend/start.js
+++ backend/start.js
@@ -6,8 +6,7 @@ require('./models/Book');
 require('./models/User');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookverse', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
