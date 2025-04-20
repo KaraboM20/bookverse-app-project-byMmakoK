86d24d32d6e49f393d99cd5260c9d66f132a8244
KaraboMmako <karabommakokm20@gmail.com>
2025-04-20 19:35:49 +0200

    Updated the start.js

--- backend/start.js
+++ backend/start.js
@@ -7,6 +7,8 @@ require('./models/Book');
 require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, 
   socketTimeoutMS: 45000,
 })
